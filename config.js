ac579dde69d299512985541870570ee9a3202632
redfisharthur-beep <redfisharthur@gmail.com>
2026-09-06 22:23:06 +0800

    Keep config data-only after direct GO markup fix

--- config.js
+++ config.js
@@ -4,9 +4,3 @@ window.CHOICE_CONFIG={
   lineOfficialUrl:"https://lin.ee/t5BkC6O",
   lineCommunityUrl:"https://line.me/ti/g2/k29Jk_pZmJCCqt5jUZjxOLnv8RNnabWLeuFu4Q?utm_source=invitation&utm_medium=link_copy&utm_campaign=default"
 };
-
-const homeGoImage=document.querySelector('#homeContinueBtn img');
-if(homeGoImage){
-  homeGoImage.src='./assets/hero/go.png';
-  homeGoImage.alt='GO';
-}
