2256038743334c59df10239cef0c2ec936596fa8
redfisharthur-beep <redfisharthur@gmail.com>
2026-09-06 11:50:56 +0800

    Use same-origin Choice API

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.CHOICE_CONFIG={
-  apiBase:"https://choice.redfisharthur.workers.dev",
+  apiBase:"",
   realtimeEnabled:false,
   lineOfficialUrl:"https://lin.ee/t5BkC6O",
   lineCommunityUrl:"https://line.me/ti/g2/k29Jk_pZmJCCqt5jUZjxOLnv8RNnabWLeuFu4Q?utm_source=invitation&utm_medium=link_copy&utm_campaign=default"
