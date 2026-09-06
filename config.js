aaec4a39a9111fe2a84da1cda4db3c4cdba288d1
redfisharthur-beep <redfisharthur@gmail.com>
2026-09-06 21:55:02 +0800

    Force home login button to use go.png

--- config.js
+++ config.js
@@ -4,13 +4,12 @@ window.CHOICE_CONFIG={
   lineOfficialUrl:"https://lin.ee/t5BkC6O",
   lineCommunityUrl:"https://line.me/ti/g2/k29Jk_pZmJCCqt5jUZjxOLnv8RNnabWLeuFu4Q?utm_source=invitation&utm_medium=link_copy&utm_campaign=default"
 };
-
 window.addEventListener('load',()=>{
   const loginGoBtn=document.querySelector('#homeLoginGoBtn');
   const loginGoImg=loginGoBtn?.querySelector('img');
   if(loginGoImg){
-    loginGoImg.src='./assets/hero/go.png';
-    loginGoImg.alt='開始';
+    loginGoImg.src='./assets/hero/go.png?v=202609062154';
+    loginGoImg.alt='GO';
   }
 
   const load=(src,key)=>new Promise(resolve=>{
