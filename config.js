099a598bc0f8fbe27c0aba113aaca6696ad84aff
redfisharthur-beep <redfisharthur@gmail.com>
2026-09-06 22:20:31 +0800

    Use GO image immediately without runtime patch loaders

--- config.js
+++ config.js
@@ -5,10 +5,8 @@ window.CHOICE_CONFIG={
   lineCommunityUrl:"https://line.me/ti/g2/k29Jk_pZmJCCqt5jUZjxOLnv8RNnabWLeuFu4Q?utm_source=invitation&utm_medium=link_copy&utm_campaign=default"
 };
 
-window.addEventListener('DOMContentLoaded',()=>{
-  const img=document.querySelector('#homeContinueBtn img');
-  if(img){
-    img.src='./assets/hero/go.png';
-    img.alt='GO';
-  }
-});
+const homeGoImage=document.querySelector('#homeContinueBtn img');
+if(homeGoImage){
+  homeGoImage.src='./assets/hero/go.png';
+  homeGoImage.alt='GO';
+}
