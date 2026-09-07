55e0d083b83be172e8446421c9b9eca2072e7637
redfisharthur-beep <redfisharthur@gmail.com>
2026-09-07 11:22:57 +0800

    Remove lobby patch loader and keep config clean

--- config.js
+++ config.js
@@ -4,10 +4,3 @@ window.CHOICE_CONFIG={
   lineOfficialUrl:"https://lin.ee/t5BkC6O",
   lineCommunityUrl:"https://line.me/ti/g2/k29Jk_pZmJCCqt5jUZjxOLnv8RNnabWLeuFu4Q?utm_source=invitation&utm_medium=link_copy&utm_campaign=default"
 };
-
-(()=>{
-  const s=document.createElement('script');
-  s.src='./lobby.js';
-  s.defer=true;
-  document.head.appendChild(s);
-})();
